3829b93b60bee86d2347fb0205d475e58635d584
vodapermadi <yapsitusaya111@gmail.com>
2022-04-16 22:32:56 +0700

    update

--- regresi-linier-ganda/main.py
+++ regresi-linier-ganda/main.py
@@ -1,57 +1,81 @@
-# rumus find Y
-# b = ((n * sXY) - (sX * sY)) / (n * sX2 - (sX ** 2))
-# a = (sY - b * sX) / n
-# y = a + bX
+# regresion multiple
 
-# table penolong
-# x1,x2,x12,x22,x1x2,y,y2
+# input data
+x1_data = str(input('input x1 : '))
+x2_data = str(input('input x2 : '))
+y_data  = str(input('input y  : '))
 
-def findY(n,sX1Y,sX2Y,sX1,sX2,sY1,sX12,sX22,xUser,xUser2):
-    b1 = ((n * sX1Y) - (sX1 * sY1)) / (n * sX12 - (sX1 ** 2))
-    b2 = ((n * sX2Y) - (sX2 * sY2)) / (n * sX22 - (sX2 ** 2))
-    a = ((sY1 / n ) - (b1 * (sX1 / n)) - (b2 * (sX2 / n)))
-    y = a + (b1 * xUser) + (b2 * xUser2)
-    print('data Y adalah: ')
+x1_pre = int(input('input x1 prediksi : '))
+x2_pre = int(input('input x2 prediksi : '))
 
-x = str(input('masukan nilai x1 : '))
-xx = str(input('masukan nilai x2 : '))
-y = str(input('masukan nilai y : '))
-user1 = int(input('data pre var 1 :'))
-user2= int(input('data pre var 2 :'))
+# split data with (,)
+cy  = y_data.split(',')
+cx1 = x1_data.split(',')
+cx2 = x2_data.split(',')
 
-kx = x.split(',')
-kxx = xx.split(',')
-ky = y.split(',')
+# casting data from str to int
+y  = [int(y_data) for y_data in cy]
+x1 = [int(x1_data) for x1_data in cx1]
+x2 = [int(x2_data) for x2_data in cx2]
 
-x1 = [int(x)for x in kx]
-x2 = [int(xx)for xx in kxx]
-y1 = [int(y)for y in ky]
-
-x12 = []
-x22 = []
+# looping for find x1y,x2y,x1x2,x12,x22
+x1y  = []
+x2y  = []
 x1x2 = []
-x1y = []
-x2y = []
-y2 = []
+x12  = []
+x22  = []
 
-for i in range(len(x1)):
-    x12 += [x1[i] ** 2]
-    x22 += [x2[i] ** 2]
-    x1x2 += [x1[i] * x2[i]]
-    x1y += [x1[i] * y1[i]]
-    x1y += [x2[i] * y1[i]]
-    y2 += [y1[i] ** 2]
+for i in range (len(x1)):
+	x1y.append(x1[i] *  y[i])
+	x2y.append(x2[i] *  y[i])
+	x1x2.append(x1[i] * x2[i])
+	x12.append(x1[i] ** 2)
+	x22.append(x2[i] ** 2)
 
-# sigma
-n = sum(x1)
-sX1 = sum(x1)
-sX12 = sum(x12)
-sX2 = sum(x2)
-sX22 = sum(x22)
+# find sigma
+n     = len(y)
+sY    = sum(y)
+sX1   = sum(x1)
+sX2   = sum(x2)
+sX1Y  = sum(x1y)
+sX2Y  = sum(x2y)
 sX1X2 = sum(x1x2)
-sX1Y = sum(x1y)
-sX2Y = sum(x2y)
-sY1 = sum(y1)
-sY2 = sum(y2)
+sX12  = sum(x12)
+sX22  = sum(x22)
 
-findY(n,sX1Y,sX2Y,sX1,sX2,sY1,sX12,sX22,user1,user2)
+#find matrix A
+#            [  n  | sX1  |  sX2 ] | [  n  ][  sX1  ]
+# matrix A = [ sX1 | sX12 | sX1X2] | [ sX1 ][  sX12 ]
+#            [ sX2 | sX1X2| sX22 ] | [ sX2 ][  sX1X2]
+
+#             [  sY  | sX1  |  sX2 ] | [  sY  ][  sX1  ]
+# matrix A1 = [ sX1Y | sX12 | sX1X2] | [ sX1Y ][  sX12 ]
+#             [ sX2Y | sX1X2| sX22 ] | [ sX2Y ][  sX1X2]
+
+#             [  n  | sY   |  sX2 ] | [  n  ][  sY   ]
+# matrix A2 = [ sX1 | sX1Y | sX1X2] | [ sX1 ][  sX1Y ]
+#             [ sX2 | sX2Y | sX22 ] | [ sX2 ][  sX2Y ]
+
+#             [  n  | sX1  |  sY  ] | [  n  ][  sX1  ]
+# matrix A3 = [ sX1 | sX12 | sX1Y ] | [ sX1 ][  sX12 ]
+#             [ sX2 | sX1X2| sX2Y ] | [ sX2 ][  sX1X2]
+
+#            [  sY  ]
+# matrix H = [ sX1Y ]
+#            [ sX2Y ]
+
+det_a  = ((n * sX12 * sX22) + (sX1 * sX1X2 * sX2) + (sX2 * sX1 * sX1X2) - (sX2 * sX12 * sX2) - (n * (sX1X2 ** 2)) - ((sX1 ** 2) * sX22))
+det_a1 = ((sY * sX12 * sX22) + (sX1 * sX1X2 * sX2Y) + (sX2 * sX1Y * sX1X2) - (sX2 * sX12 * sX2Y) - (sY * (sX1X2 ** 2)) - (sX1 * sX1Y * sX22))
+det_a2 = ((n * sX1Y * sX22) + (sY * sX1X2 * sX2) + (sX2 * sX1 * sX2Y) - (sX2 * sX1Y * sX2) - (n * sX1X2 * sX2Y) - (sY * sX1 * sX22))
+det_a3 = ((n * sX12 * sX2Y) + (sX1 * sX1Y * sX2) + (sY * sX1 * sX1X2) - (sY * sX12 * sX2) - (n * sX1Y * sX1X2) - ((sX1 ** 2) * sX2Y))
+
+# find b
+b1 = det_a1 / det_a
+b2 = det_a2 / det_a
+b3 = det_a3 / det_a
+
+# regresion multiple
+find_y = b1 + (b2 * x1_pre) + (b3 * x2_pre)
+
+# deploy
+print(find_y)
--- regresi-linier-sederhana/main.py
+++ regresi-linier-sederhana/main.py
@@ -7,23 +7,30 @@ def findY(n,sX,sY,sXY,sX2,inputUser):
     a = (sY - b * sX) / n
     print('nilai y prediksi : ', a + b * inputUser)
 
+# memasukan input menjadi str
 x = str(input('masukan nilai x : '))
 y = str(input('masukan nilai y : '))
-xUser = float(input('nilai yang di prediksi :'))
+xUser = float(input('x prediksi : '))
 
+# memisahkan data dengan menggunakan koma
+cx = x.split(',')
+cy = y.split(',')
+
+# memasukan hasil split dan mengubah type data string ke integer
+x1 = [int(x) for x in cx]
+y1 = [int(y) for y in cy]
+
+# looping untuk menjumlahkan isi setiap element array
 x2 = []
 y2 = []
 xy = []
-cx = x.split(',')
-cy = y.split(',')
-x1 = [int(x) for x in cx]
-y1 = [int(y) for y in cy]
 
 for i in range (len(x1)):
     x2 += [x1[i] ** 2]
     y2 += [y1[i] ** 2]
     xy += [x1[i] * y1[i]]
 
+# s adalah kepanjangan dari sigma dan n adalah banyk data dalam suatu array
 n   = len(x1)
 sX  = sum(x1)
 sY  = sum(y1)
@@ -31,5 +38,6 @@ sX2 = sum(x2)
 sY2 = sum(y2)
 sXY = sum(xy)
 
+# eksekusi kode dengan memanggil fuction
 korelasi(n,sX,sX2,sY,sY2,sXY)
 findY(n,sX,sY,sXY,sX2,xUser)
